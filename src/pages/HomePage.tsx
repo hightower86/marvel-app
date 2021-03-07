901ec7c1630da7a519aa132bdb285c368ff3b3cf
Adel <htwr86@gmail.com>
2021-03-07 18:08:48 +0300

    axios request from Marvel

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
 
 }
 
 const HomePage = (props: Props) => {
+
+  const [todos, setTodos] = useState({})
+
+  useEffect(() => {
+    const request = async () => {
+      try {
+        const params = {
+          limit: 10,
+          apikey: '1bded6638cafa565ae83ba51bc9e1d43'
+        }
+        const { data } = await axios('http://gateway.marvel.com/v1/public/comics', { params })
+        console.log({ data });
+        setTodos(data)
+      } catch (error) {
+        console.log({ error });
+      }
+
+    }
+
+    request()
+
+  }, [])
   return (
     <div>
       Home sweet home
+      <pre>{JSON.stringify(todos, null, 4)}</pre>
     </div>
   )
 }
